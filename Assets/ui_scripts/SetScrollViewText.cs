fc0b08c239bdadcb7bc0589aebbd079edfc774b5
Trixton <veritwixt@gmail.com>
2020-05-12 06:00:32 -0500

    update

---
+++ Assets/ui_scripts/SetScrollViewText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SetScrollViewText : MonoBehaviour
+{
+    
+
+    void SetTextOnClick(string text)
+    {
+        //scrollView.content  = "testeroni pizzi is weak but tastes good";
+    }
+}
