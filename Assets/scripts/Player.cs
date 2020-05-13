a456049a24c29e92009e3306dc07a120ee2514d4
Trixton <veritwixt@gmail.com>
2020-05-13 08:40:26 -0500

    adding more to the playground

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform transform;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerSpeed = Time.deltaTime * speed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, playerSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -playerSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-playerSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(playerSpeed, 0, 0);
+        }
+    }
+}
