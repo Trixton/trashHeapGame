c0d2f6c359ca00a4420b9784afee4967ed202869
Trixton <veritwixt@gmail.com>
2020-05-15 00:09:36 -0500

    added ECS to project

---
+++ Assets/scripts/ECS/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // EntityManager entityManager = World.Active.EntityManager;   
+        // entityManager.CreateEntity();
+    }
+}
